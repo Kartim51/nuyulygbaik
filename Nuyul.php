56135d66aa52945e9cf2f331a189da1f6fdb352b
L0L1_30H4Y <59199190+Kartim51@users.noreply.github.com>
2019-12-24 18:55:50 +0700

    Nuyul.php

---
+++ Nuyul.php
@@ -0,0 +1,15 @@
+Halo!
+Perkenalkan nama saya L0L1_30H4Y
+
+Disini saya akan memberikan tutorial cara
+Nuyul yodo pertama kalian siap kan akun yodo dan apk termux
+Langkah pertama di termux nya 
+
+$ pkg update && pkg upgrade 
+$ pkg install PHP
+$ pkg install hit
+$ pkg install figlet
+$ pkg install python2
+$ git clone https://github.com/Kartim51/nuyulygbaik
+$ cd Nuyul.php
+$ php Nuyul.php
